b45d1775b8d5d8142219535dd4adc30d7c4496b9
Muliro1 <mulirokhaemba@gmail.com>
2025-07-31 00:31:11 +0300

    fx

--- polling_system/polling_system/settings.py
+++ polling_system/polling_system/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'polling_system.urls'
@@ -141,8 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -165,3 +165,8 @@ SWAGGER_SETTINGS = {
 GRAPHENE = {
     "SCHEMA": "polls.schema.schema"  # We'll create this file next
 }
+
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
